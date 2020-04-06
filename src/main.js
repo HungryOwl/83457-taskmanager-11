12712c3e67ab10c39834abb417131ae5022c9470
anovikova <ptica-govorun@inbox.ru>
2020-04-06 03:22:36 +0300

    Улучшаем throw error в getHtmlElement

--- src/main.js
+++ src/main.js
@@ -12,7 +12,7 @@ const getHtmlElement = (className) => {
   const element = document.querySelector(`.${className}`);
 
   if (!element) {
-    throw new Error(`Элемент не найден`);
+    throw new Error(`Элемент ${className} не найден`);
   }
 
   return element;
